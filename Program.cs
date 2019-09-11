380ae12df3ec6adfdf3563076ef4acb2db1e102d
dhruvdua6300 <dhruvdua@mail.usf.edu>
2019-09-10 20:08:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace My_first_program
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many years experience do you have in professional programming?");
+        int years = int.Parse(Console.ReadLine());
+
+        {
+            if (years == 0)
+
+                Console.WriteLine("Need lots of practice");
+
+            if (years == 1)
+
+                Console.WriteLine("Looks like you have a little experience");
+
+            if (years == 2)
+
+                Console.WriteLine("Wow! You've been doing this for a little while");
+
+            if (years > 2)
+
+                Console.WriteLine("You are an expert!");
+
+
+        }
+
+
+
+
+    }
+}
+}
+
